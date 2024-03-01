14782af058bccf717e8cdda0af8b947f0c17241b
iliveinthepresent <qycqiuyunchang@foxmail.com>
2024-03-01 23:33:11 +0800

    升级为命令行代码生成器

---
+++ Q-code-generator-basic/src/main/java/com/qiu/cli/CommandExecutor.java
@@ -0,0 +1,35 @@
+package com.qiu.cli;
+
+import com.qiu.cli.command.ConfigCommand;
+import com.qiu.cli.command.GenerateCommand;
+import com.qiu.cli.command.ListCommand;
+import picocli.CommandLine;
+
+@CommandLine.Command(name = "qiu", mixinStandardHelpOptions = true)
+public class CommandExecutor implements Runnable {
+
+    private final CommandLine commandLine;
+
+    {
+        commandLine = new CommandLine(this)
+                .addSubcommand(new GenerateCommand())
+                .addSubcommand(new ConfigCommand())
+                .addSubcommand(new ListCommand());
+    }
+
+    @Override
+    public void run() {
+        // 不输入子命令时，给出友好提示
+        System.out.println("请输入具体命令，或者输入 --help 查看命令提示");
+    }
+
+    /**
+     * 执行命令
+     *
+     * @param args
+     * @return
+     */
+    public Integer doExecute(String[] args) {
+        return commandLine.execute(args);
+    }
+}
