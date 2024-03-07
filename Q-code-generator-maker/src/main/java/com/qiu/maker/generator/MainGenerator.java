a970b53b76a37ef383454c68ec9fb88ea3a479c3
iliveinthepresent <qycqiuyunchang@foxmail.com>
2024-03-07 23:38:02 +0800

    1.生成代码生成器时，将原始代码模板，复制到生成器中，实现可移植性。 2.生成代码生成器时，生成README.md文件

--- Q-code-generator-maker/src/main/java/com/qiu/maker/generator/MainGenerator.java
+++ Q-code-generator-maker/src/main/java/com/qiu/maker/generator/MainGenerator.java
@@ -27,6 +27,11 @@ public class MainGenerator {
             FileUtil.mkdir(outputPath);
         }
 
+        // 复制原始文件
+        String sourceRootPath = meta.getFileConfig().getSourceRootPath();
+        String sourceDestPath = outputPath + File.separator + ".source";
+        FileUtil.copy(sourceRootPath, sourceDestPath, false);
+
         // 读取 resources 目录
         ClassPathResource classPathResource = new ClassPathResource("");
         String inputResourcePath = classPathResource.getAbsolutePath();
@@ -87,6 +92,11 @@ public class MainGenerator {
         // pom.xml
         inputFilePath = inputResourcePath + File.separator + "templates/pom.xml.ftl";
         outputFilePath = outputPath + File.separator + "pom.xml";
+        DynamicFileGenerator.doGenerate(inputFilePath , outputFilePath, meta);
+
+        // README.md
+        inputFilePath = inputResourcePath + File.separator + "templates/README.md.ftl";
+        outputFilePath = outputPath + File.separator + "README.md";
         DynamicFileGenerator.doGenerate(inputFilePath , outputFilePath, meta);
 
         // 构建 jar 包
