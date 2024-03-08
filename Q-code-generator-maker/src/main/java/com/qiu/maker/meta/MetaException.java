27a01ccd4329d5747e837c870943dcf9ad1ada0b
iliveinthepresent <qycqiuyunchang@foxmail.com>
2024-03-08 21:16:00 +0800

    实现元信息的校验和默认值填充

---
+++ Q-code-generator-maker/src/main/java/com/qiu/maker/meta/MetaException.java
@@ -0,0 +1,14 @@
+package com.qiu.maker.meta;
+
+/**
+ * 元信息异常
+ */
+public class MetaException extends RuntimeException {
+    public MetaException(String message) {
+        super(message);
+    }
+
+    public MetaException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
