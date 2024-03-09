d9921011aca779cd00a12aca7170c3bb2e0d561d
iliveinthepresent <qycqiuyunchang@foxmail.com>
2024-03-09 15:28:21 +0800

    定义枚举值，替换魔法值

--- Q-code-generator-maker/src/main/java/com/qiu/maker/meta/MetaValidator.java
+++ Q-code-generator-maker/src/main/java/com/qiu/maker/meta/MetaValidator.java
@@ -4,8 +4,10 @@ import cn.hutool.core.collection.CollectionUtil;
 import cn.hutool.core.date.DateUtil;
 import cn.hutool.core.io.FileUtil;
 import cn.hutool.core.util.StrUtil;
+import com.qiu.maker.meta.enums.FileGenerateTypeEnum;
+import com.qiu.maker.meta.enums.FileTypeEnum;
+import com.qiu.maker.meta.enums.ModelTypeEnum;
 
-import java.io.File;
 import java.nio.file.Paths;
 import java.util.List;
 
@@ -39,7 +41,7 @@ public class MetaValidator {
 
             String modelInfoType = modelInfo.getType();
             if (StrUtil.isEmpty(modelInfoType)) {
-                modelInfo.setType("String");
+                modelInfo.setType(ModelTypeEnum.STRING.getValue());
             }
         }
     }
@@ -68,7 +70,7 @@ public class MetaValidator {
             fileConfig.setOutputRootPath(defaultOutputRootPath);
         }
         String fileConfigType = fileConfig.getType();
-        String defaultType = "dir";
+        String defaultType = FileTypeEnum.DIR.getValue();
         if (StrUtil.isEmpty(fileConfigType)) {
             fileConfig.setType(defaultType);
         }
@@ -95,9 +97,9 @@ public class MetaValidator {
             if (StrUtil.isBlank(type)) {
                 // 无文件后缀
                 if (StrUtil.isBlank(FileUtil.getSuffix(inputPath))) {
-                    fileInfo.setType("dir");
+                    fileInfo.setType(FileTypeEnum.DIR.getValue());
                 } else {
-                    fileInfo.setType("file");
+                    fileInfo.setType(FileTypeEnum.FILE.getValue());
                 }
             }
             // generateType：如果文件结尾不为 Ftl，generateType 默认为 static，否则为 dynamic
@@ -105,9 +107,9 @@ public class MetaValidator {
             if (StrUtil.isBlank(generateType)) {
                 // 为动态模板
                 if (inputPath.endsWith(".ftl")) {
-                    fileInfo.setGenerateType("dynamic");
+                    fileInfo.setGenerateType(FileGenerateTypeEnum.DYNAMIC.getValue());
                 } else {
-                    fileInfo.setGenerateType("static");
+                    fileInfo.setGenerateType(FileGenerateTypeEnum.STATIC.getValue());
                 }
             }
         }
