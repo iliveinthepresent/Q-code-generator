f4ba12546a4b9764a002e3228b908a215479c2ad
iliveinthepresent <qycqiuyunchang@foxmail.com>
2024-03-09 15:16:04 +0800

    降低校验填充元信息方法的圈复杂度（抽取方法、卫语句、封装工具类）

--- Q-code-generator-maker/src/main/java/com/qiu/maker/meta/MetaValidator.java
+++ Q-code-generator-maker/src/main/java/com/qiu/maker/meta/MetaValidator.java
@@ -15,128 +15,117 @@ import java.util.List;
 public class MetaValidator {
 
     public static void doValidAndFill(Meta meta) {
-        // 基础信息校验和默认值
-        String name = meta.getName();
-        if (StrUtil.isBlank(name)) {
-            name = "my-generator";
-            meta.setName(name);
-        }
-
-        String description = meta.getDescription();
-        if (StrUtil.isEmpty(description)) {
-            description = "我的模板代码生成器";
-            meta.setDescription(description);
-        }
-
-        String author = meta.getAuthor();
-        if (StrUtil.isEmpty(author)) {
-            author = "qiu";
-            meta.setAuthor(author);
-        }
-
-        String basePackage = meta.getBasePackage();
-        if (StrUtil.isBlank(basePackage)) {
-            basePackage = "com.qiu";
-            meta.setBasePackage(basePackage);
-        }
-
-        String version = meta.getVersion();
-        if (StrUtil.isEmpty(version)) {
-            version = "1.0";
-            meta.setVersion(version);
-        }
-
-        String createTime = meta.getCreateTime();
-        if (StrUtil.isEmpty(createTime)) {
-            createTime = DateUtil.now();
-            meta.setCreateTime(createTime);
-        }
-
-        // fileConfig 校验和默认值
-        Meta.FileConfig fileConfig = meta.getFileConfig();
-        if (fileConfig != null) {
-            // sourceRootPath：必填
-            String sourceRootPath = fileConfig.getSourceRootPath();
-            if (StrUtil.isBlank(sourceRootPath)) {
-                throw new MetaException("未填写 sourceRootPath");
-            }
-            // inputRootPath：.source + sourceRootPath 的最后一个层级路径
-            String inputRootPath = fileConfig.getInputRootPath();
-            String defaultInputRootPath = ".source" + File.separator + FileUtil.getLastPathEle(Paths.get(sourceRootPath)).getFileName().toString();
-            if (StrUtil.isEmpty(inputRootPath)) {
-                fileConfig.setInputRootPath(defaultInputRootPath);
-            }
-            // outputRootPath：默认为当前路径下的 generated
-            String outputRootPath = fileConfig.getOutputRootPath();
-            String defaultOutputRootPath = "generated";
-            if (StrUtil.isEmpty(outputRootPath)) {
-                fileConfig.setOutputRootPath(defaultOutputRootPath);
-            }
-            String fileConfigType = fileConfig.getType();
-            String defaultType = "dir";
-            if (StrUtil.isEmpty(fileConfigType)) {
-                fileConfig.setType(defaultType);
-            }
-
-            // fileInfo 默认值
-            List<Meta.FileConfig.FileInfo> fileInfoList = fileConfig.getFiles();
-            if (CollectionUtil.isNotEmpty(fileInfoList)) {
-                for (Meta.FileConfig.FileInfo fileInfo : fileInfoList) {
-                    // inputPath: 必填
-                    String inputPath = fileInfo.getInputPath();
-                    if (StrUtil.isBlank(inputPath)) {
-                        throw new MetaException("未填写 inputPath");
-                    }
-
-                    // outputPath: 默认等于 inputPath
-                    String outputPath = fileInfo.getOutputPath();
-                    if (StrUtil.isEmpty(outputPath)) {
-                        fileInfo.setOutputPath(inputPath);
-                    }
-                    // type：默认 inputPath 有文件后缀（如 .java）为 file，否则为 dir
-                    String type = fileInfo.getType();
-                    if (StrUtil.isBlank(type)) {
-                        // 无文件后缀
-                        if (StrUtil.isBlank(FileUtil.getSuffix(inputPath))) {
-                            fileInfo.setType("dir");
-                        } else {
-                            fileInfo.setType("file");
-                        }
-                    }
-                    // generateType：如果文件结尾不为 Ftl，generateType 默认为 static，否则为 dynamic
-                    String generateType = fileInfo.getGenerateType();
-                    if (StrUtil.isBlank(generateType)) {
-                        // 为动态模板
-                        if (inputPath.endsWith(".ftl")) {
-                            fileInfo.setGenerateType("dynamic");
-                        } else {
-                            fileInfo.setGenerateType("static");
-                        }
-                    }
-                }
-            }
-        }
-
-        // modelConfig 校验和默认值
-        Meta.ModelConfig modelConfig = meta.getModelConfig();
-        if (modelConfig != null) {
-            List<Meta.ModelConfig.ModelInfo> modelInfoList = modelConfig.getModels();
-            if (CollectionUtil.isNotEmpty(modelInfoList)) {
-                for (Meta.ModelConfig.ModelInfo modelInfo : modelInfoList) {
-                    // 输出路径默认值
-                    String fieldName = modelInfo.getFieldName();
-                    if (StrUtil.isBlank(fieldName)) {
-                        throw new MetaException("未填写 fieldName");
-                    }
-
-                    String modelInfoType = modelInfo.getType();
-                    if (StrUtil.isEmpty(modelInfoType)) {
-                        modelInfo.setType("String");
-                    }
-                }
-            }
-        }
-
+        validAndFillMetaRoot(meta);
+        validAndFillFileConfig(meta);
+        validAndFillModelConfig(meta);
     }
 
+    public static void validAndFillModelConfig(Meta meta) {
+        Meta.ModelConfig modelConfig = meta.getModelConfig();
+        if (modelConfig == null) {
+            return;
+        }
+        // modelConfig 默认值
+        List<Meta.ModelConfig.ModelInfo> modelInfoList = modelConfig.getModels();
+        if (!CollectionUtil.isNotEmpty(modelInfoList)) {
+            return;
+        }
+        for (Meta.ModelConfig.ModelInfo modelInfo : modelInfoList) {
+            // 输出路径默认值
+            String fieldName = modelInfo.getFieldName();
+            if (StrUtil.isBlank(fieldName)) {
+                throw new MetaException("未填写 fieldName");
+            }
+
+            String modelInfoType = modelInfo.getType();
+            if (StrUtil.isEmpty(modelInfoType)) {
+                modelInfo.setType("String");
+            }
+        }
+    }
+
+    public static void validAndFillFileConfig(Meta meta) {
+        // fileConfig 默认值
+        Meta.FileConfig fileConfig = meta.getFileConfig();
+        if (fileConfig == null) {
+            return;
+        }
+        // sourceRootPath：必填
+        String sourceRootPath = fileConfig.getSourceRootPath();
+        if (StrUtil.isBlank(sourceRootPath)) {
+            throw new MetaException("未填写 sourceRootPath");
+        }
+        // inputRootPath：.source + sourceRootPath 的最后一个层级路径
+        String inputRootPath = fileConfig.getInputRootPath();
+        String defaultInputRootPath = ".source/" + FileUtil.getLastPathEle(Paths.get(sourceRootPath)).getFileName().toString();
+        if (StrUtil.isEmpty(inputRootPath)) {
+            fileConfig.setInputRootPath(defaultInputRootPath);
+        }
+        // outputRootPath：默认为当前路径下的 generated
+        String outputRootPath = fileConfig.getOutputRootPath();
+        String defaultOutputRootPath = "generated";
+        if (StrUtil.isEmpty(outputRootPath)) {
+            fileConfig.setOutputRootPath(defaultOutputRootPath);
+        }
+        String fileConfigType = fileConfig.getType();
+        String defaultType = "dir";
+        if (StrUtil.isEmpty(fileConfigType)) {
+            fileConfig.setType(defaultType);
+        }
+
+        // fileInfo 默认值
+        List<Meta.FileConfig.FileInfo> fileInfoList = fileConfig.getFiles();
+        if (!CollectionUtil.isNotEmpty(fileInfoList)) {
+            return;
+        }
+        for (Meta.FileConfig.FileInfo fileInfo : fileInfoList) {
+            // inputPath: 必填
+            String inputPath = fileInfo.getInputPath();
+            if (StrUtil.isBlank(inputPath)) {
+                throw new MetaException("未填写 inputPath");
+            }
+
+            // outputPath: 默认等于 inputPath
+            String outputPath = fileInfo.getOutputPath();
+            if (StrUtil.isEmpty(outputPath)) {
+                fileInfo.setOutputPath(inputPath);
+            }
+            // type：默认 inputPath 有文件后缀（如 .java）为 file，否则为 dir
+            String type = fileInfo.getType();
+            if (StrUtil.isBlank(type)) {
+                // 无文件后缀
+                if (StrUtil.isBlank(FileUtil.getSuffix(inputPath))) {
+                    fileInfo.setType("dir");
+                } else {
+                    fileInfo.setType("file");
+                }
+            }
+            // generateType：如果文件结尾不为 Ftl，generateType 默认为 static，否则为 dynamic
+            String generateType = fileInfo.getGenerateType();
+            if (StrUtil.isBlank(generateType)) {
+                // 为动态模板
+                if (inputPath.endsWith(".ftl")) {
+                    fileInfo.setGenerateType("dynamic");
+                } else {
+                    fileInfo.setGenerateType("static");
+                }
+            }
+        }
+    }
+
+    public static void validAndFillMetaRoot(Meta meta) {
+        // 校验并填充默认值
+        String name = StrUtil.blankToDefault(meta.getName(), "my-generator");
+        String description = StrUtil.emptyToDefault(meta.getDescription(), "我的模板代码生成器");
+        String author = StrUtil.emptyToDefault(meta.getAuthor(), "yupi");
+        String basePackage = StrUtil.blankToDefault(meta.getBasePackage(), "com.yupi");
+        String version = StrUtil.emptyToDefault(meta.getVersion(), "1.0");
+        String createTime = StrUtil.emptyToDefault(meta.getCreateTime(), DateUtil.now());
+        meta.setName(name);
+        meta.setDescription(description);
+        meta.setAuthor(author);
+        meta.setBasePackage(basePackage);
+        meta.setVersion(version);
+        meta.setCreateTime(createTime);
+    }
 }
