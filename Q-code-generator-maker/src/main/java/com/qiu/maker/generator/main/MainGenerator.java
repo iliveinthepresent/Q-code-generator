3455d068e4509757ccb6d1637ebc3a8b60f71a23
iliveinthepresent <qycqiuyunchang@foxmail.com>
2024-03-09 16:06:43 +0800

    使用模板方法模式优化MainGenerator的复杂流程，提高可扩展性

---
+++ Q-code-generator-maker/src/main/java/com/qiu/maker/generator/main/MainGenerator.java
@@ -0,0 +1,11 @@
+package com.qiu.maker.generator.main;
+
+import freemarker.template.TemplateException;
+
+import java.io.IOException;
+
+/**
+ * 生成数据模型
+ */
+public class MainGenerator extends GenerateTemplate{
+}
