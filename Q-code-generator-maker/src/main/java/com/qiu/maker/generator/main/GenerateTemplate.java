43c046d1f3e2c76c486fc1a1041b61fc4a6eaf29
iliveinthepresent <qycqiuyunchang@foxmail.com>
2024-03-10 22:34:26 +0800

    增强制作工具的配置能力，使其支持更灵活的配置

--- Q-code-generator-maker/src/main/java/com/qiu/maker/generator/main/GenerateTemplate.java
+++ Q-code-generator-maker/src/main/java/com/qiu/maker/generator/main/GenerateTemplate.java
@@ -114,6 +114,11 @@ public abstract class GenerateTemplate {
         outputFilePath = outputBaseJavaPackagePath + "/model/DataModel.java";
         DynamicFileGenerator.doGenerate(inputFilePath , outputFilePath, meta);
 
+        // generator.MainGenerator
+        inputFilePath = inputResourcePath + File.separator + "templates/java/generator/MainGenerator.java.ftl";
+        outputFilePath = outputBaseJavaPackagePath + "/generator/MainGenerator.java";
+        DynamicFileGenerator.doGenerate(inputFilePath , outputFilePath, meta);
+
         // cli.command.ConfigCommand
         inputFilePath = inputResourcePath + File.separator + "templates/java/cli/command/ConfigCommand.java.ftl";
         outputFilePath = outputBaseJavaPackagePath + "/cli/command/ConfigCommand.java";
@@ -142,11 +147,6 @@ public abstract class GenerateTemplate {
         // generator.DynamicGenerator
         inputFilePath = inputResourcePath + File.separator + "templates/java/generator/DynamicGenerator.java.ftl";
         outputFilePath = outputBaseJavaPackagePath + "/generator/DynamicGenerator.java";
-        DynamicFileGenerator.doGenerate(inputFilePath , outputFilePath, meta);
-
-        // generator.MainGenerator
-        inputFilePath = inputResourcePath + File.separator + "templates/java/generator/MainGenerator.java.ftl";
-        outputFilePath = outputBaseJavaPackagePath + "/generator/MainGenerator.java";
         DynamicFileGenerator.doGenerate(inputFilePath , outputFilePath, meta);
 
         // generator.StaticGenerator
--- Q-code-generator-maker/src/main/java/com/qiu/maker/generator/main/MainGenerator.java
+++ Q-code-generator-maker/src/main/java/com/qiu/maker/generator/main/MainGenerator.java
@@ -8,4 +8,8 @@ import java.io.IOException;
  * 生成数据模型
  */
 public class MainGenerator extends GenerateTemplate{
+    public static void main(String[] args) throws TemplateException, IOException, InterruptedException {
+        MainGenerator mainGenerator = new MainGenerator();
+        mainGenerator.doGenerate();
+    }
 }
