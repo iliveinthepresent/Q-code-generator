9ae0cb804253a67d839cdab71ccd46dcc86202a6
iliveinthepresent <qycqiuyunchang@foxmail.com>
2024-03-14 20:18:20 +0800

    添加示例项目

---
+++ Q-code-generator-demo-projects/springboot-init/src/main/java/com/qiu/springbootinit/mapper/PostMapper.java
@@ -0,0 +1,15 @@
+package com.qiu.springbootinit.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.qiu.springbootinit.model.entity.Post;
+
+/**
+ * 帖子数据库操作
+ */
+public interface PostMapper extends BaseMapper<Post> {
+
+}
+
+
+
+
---
+++ Q-code-generator-demo-projects/springboot-init/src/main/java/com/qiu/springbootinit/mapper/UserMapper.java
@@ -0,0 +1,15 @@
+package com.qiu.springbootinit.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.qiu.springbootinit.model.entity.User;
+
+/**
+ * 用户数据库操作
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
